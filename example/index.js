6b6c3fb1f6399010c88a9fd3ca1cbe3891375058
cyokodog <cyokodog@gmail.com>
2016-05-25 14:27:40 +0900

    exampleのpath記述方法修正

--- example/index.js
+++ example/index.js
@@ -1,14 +1,14 @@
 const fs = require('fs-extra');
-const Docs = require('../lib/Docs');
+const Docs = require(__dirname + '/../lib/Docs');
 const docs = new Docs();
 
 docs.parse({
-  srcDoc: fs.readFileSync('./doc.md', 'utf-8')
+  srcDoc: fs.readFileSync(__dirname + '/doc.md', 'utf-8')
 });
 
 docs.render({
-  template: fs.readFileSync('./theme/template.html', 'utf-8'),
-  templatePath: './theme'
+  template: fs.readFileSync(__dirname + '/theme/template.html', 'utf-8'),
+  templatePath: __dirname + '/theme'
 });
 
 console.log('[result]', docs.getHtml());
